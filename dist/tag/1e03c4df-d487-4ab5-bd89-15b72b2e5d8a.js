a4b15a4cf12a0c1aa154adec34b64dea2c064e75
Srorking <147788561+Srorking@users.noreply.github.com>
2026-02-03 17:09:20 +0300

    new update and fix bugs version 0.2.2

--- dist/tag/1e03c4df-d487-4ab5-bd89-15b72b2e5d8a.js
+++ dist/tag/1e03c4df-d487-4ab5-bd89-15b72b2e5d8a.js
@@ -2,7 +2,6 @@
   var w = window;
   var q = (w._ptq = w._ptq || []);
 
-  // استخرج base من رابط سكربت الـtag نفسه
   var cs = document.currentScript;
   var base = "";
   if (cs && cs.src) {
@@ -12,16 +11,16 @@
   q.push(["__meta", {
     project_id: "1e03c4df-d487-4ab5-bd89-15b72b2e5d8a",
     cdnBase: base,
-    tagVersion: "0.2.1"
+    tagVersion: "0.2.2"
   }]);
 
-  // ✅ أهم سطر بالـProduction
-  q.push(["config", { collectUrl: "https://collect-api-yzug.onrender.com/collect" }]);
+  // ✅ config (collectUrl + optional plugins)
+  q.push(["config", {"collectUrl":"https://collect-api-yzug.onrender.com/collect","plugins":{"ads":true,"search":true}}]);
 
   var d = document;
   var s = d.createElement("script");
   s.async = true;
-  s.src = base + "/core/sdk.min.js?v=" + encodeURIComponent("0.2.1");
+  s.src = base + "/core/sdk.min.js?v=" + encodeURIComponent("0.2.2");
 
   var h = d.getElementsByTagName("script")[0];
   h.parentNode.insertBefore(s, h);
--- dist/tag/d2f3a4b5-c6d7-4e8f-9a0b-6c5d4e3f2a66.js
+++ dist/tag/d2f3a4b5-c6d7-4e8f-9a0b-6c5d4e3f2a66.js
@@ -2,7 +2,6 @@
   var w = window;
   var q = (w._ptq = w._ptq || []);
 
-  // استخرج base من رابط سكربت الـtag نفسه
   var cs = document.currentScript;
   var base = "";
   if (cs && cs.src) {
@@ -12,16 +11,16 @@
   q.push(["__meta", {
     project_id: "d2f3a4b5-c6d7-4e8f-9a0b-6c5d4e3f2a66",
     cdnBase: base,
-    tagVersion: "0.1.0"
+    tagVersion: "0.2.2"
   }]);
 
-  // ✅ أهم سطر بالـProduction
-  q.push(["config", { collectUrl: "http://127.0.0.1:4000/collect" }]);
+  // ✅ config (collectUrl + optional plugins)
+  q.push(["config", {"collectUrl":"https://coll"}]);
 
   var d = document;
   var s = d.createElement("script");
   s.async = true;
-  s.src = base + "/core/sdk.min.js?v=0.1.0";
+  s.src = base + "/core/sdk.min.js?v=" + encodeURIComponent("0.2.2");
 
   var h = d.getElementsByTagName("script")[0];
   h.parentNode.insertBefore(s, h);
